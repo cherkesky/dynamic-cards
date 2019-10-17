d04ab4349b4e2094c23037d0eca3b18735a682c6
Cherkesky <guy@cherkesky.com>
2019-10-17 14:41:18 -0500

    initial file structure + basic css + drafted roughly the functions

--- cards.js
+++ cards.js
@@ -1,1 +1,14 @@
 console.log("Dynamic Cards")
+
+function createComponentFactory (){
+  //component, border and delete button needs to be create here
+  console.log("CLICK")
+  uiPainter();
+}
+
+function uiPainter (){
+// print to the DOM
+console.log("CLICK2")
+}
+
+document.querySelector("#create-button").addEventListener("click",createComponentFactory)
